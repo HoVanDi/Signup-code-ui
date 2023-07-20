0c97d7e70adc3312dff4a56a42ab9cfb7e99ef72
HoVanDi <hovandideveloper@gmail.com>
2023-07-20 17:31:15 +0700

    fix space

--- assets/Script.js
+++ assets/Script.js
@@ -4,7 +4,7 @@ document.getElementById("claim-button").addEventListener("click", function () {
     var lastName = document.getElementById('last-name').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    var emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{1,1})+$/;
 
     // Xóa thông báo lỗi trước khi kiểm tra lại
     document.getElementById("first-name-error").textContent = "";
@@ -17,36 +17,44 @@ document.getElementById("claim-button").addEventListener("click", function () {
         document.getElementById("first-name-error").textContent = "First Name cannot be empty.";
         document.getElementById("first-name").classList.add("error");
         document.getElementById("first-name-icon").style.display = "inline-block";
+        document.getElementById("first-name").style.borderColor = "#FF7A7A";
     } else {
         document.getElementById("first-name").classList.remove("error");
         document.getElementById("first-name-icon").style.display = "none";
+        document.getElementById("first-name").style.borderColor = '#B9B6D3';
     }
 
     if (lastName === "") {
+        document.getElementById("last-name").style.borderColor = "#FF7A7A";
         document.getElementById("last-name-error").textContent = "Lasst Name cannot be empty.";
         document.getElementById("last-name").classList.add("error");
         document.getElementById("last-name-icon").style.display = "inline-block";
     } else {
         document.getElementById("last-name").classList.remove("error");
         document.getElementById("last-name-icon").style.display = "none";
+        document.getElementById("last-name").style.borderColor = '#B9B6D3';
     }
 
     if (email === ""  || !emailRegex.test(email)) {
+        document.getElementById("email").style.borderColor = "#FF7A7A";
         document.getElementById("email-error").textContent = "Looks like this is not an email.";
         document.getElementById("email").classList.add("error");
         document.getElementById("email-icon").style.display = "inline-block";
     } else {
         document.getElementById("email").classList.remove("error");
         document.getElementById("email-icon").style.display = "none";
+        document.getElementById("email").style.borderColor = '#B9B6D3';
     }
 
     if (password === "") {
+        document.getElementById("password").style.borderColor = "#FF7A7A";
         document.getElementById("password-error").textContent = "Passoword cannot be empty.";
         document.getElementById("password").classList.add("error");
         document.getElementById("password-icon").style.display = "inline-block";
     } else {
         document.getElementById("password").classList.remove("error");
         document.getElementById("password-icon").style.display = "none";
+        document.getElementById("password").style.borderColor = '#B9B6D3';
     }
 
     // Kiểm tra nếu không có thông báo lỗi nào xuất hiện, thực hiện chuyển hướng đến YouTube
